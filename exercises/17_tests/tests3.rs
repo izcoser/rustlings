af148d67582c24c66ec119bec12287d0d5c6db21
izcoser <76838137+izcoser@users.noreply.github.com>
2024-12-10 12:09:56 -0300

    Do tests

--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -23,27 +23,37 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::panic;
 
     #[test]
     fn correct_width_and_height() {
         // TODO: This test should check if the rectangle has the size that we
         // pass to its constructor.
         let rect = Rectangle::new(10, 20);
-        assert_eq!(todo!(), 10); // Check width
-        assert_eq!(todo!(), 20); // Check height
+        assert_eq!(rect.width, 10); // Check width
+        assert_eq!(rect.height, 20); // Check height
     }
 
     // TODO: This test should check if the program panics when we try to create
     // a rectangle with negative width.
+    // Could have used #[should_panic] decorator in both tests below. 
     #[test]
     fn negative_width() {
-        let _rect = Rectangle::new(-10, 10);
+        let result = panic::catch_unwind(|| {
+            let _rect = Rectangle::new(-10, 10);
+        });
+        
+        assert!(result.is_err())
     }
 
     // TODO: This test should check if the program panics when we try to create
     // a rectangle with negative height.
     #[test]
     fn negative_height() {
-        let _rect = Rectangle::new(10, -10);
+        let result = panic::catch_unwind(|| {
+            let _rect = Rectangle::new(-10, 10);
+        });
+        
+        assert!(result.is_err())
     }
 }
